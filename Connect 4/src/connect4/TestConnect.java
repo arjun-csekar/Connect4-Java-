eb6f5c567d45db09b9149054b1c79302258956cf
arjun-csekar <32626731+arjun-csekar@users.noreply.github.com>
2018-04-06 16:42:47 -0700

    Add files via upload

--- Connect 4/src/connect4/TestConnect.java
+++ Connect 4/src/connect4/TestConnect.java
@@ -17,22 +17,35 @@ public class TestConnect {
 		boolean play = true;
 		
 		print(table);
-		
+		boolean win = false;
 		
 		while(true) {
 			System.out.print(play ? "Player 1 " + "Enter column: " : "Player 2 " + "Enter column: ");
 			col = sc.nextInt() - 1;
 			
 			if(play) {
-				table[check(table, col)][col] = 'X';
+				int row = check(table, col);
+				table[row][col] = 'X';
+				if(checkWin(table, row, col)) {
+					System.out.println("\nWINNER!");
+					win = true;
+				}
 				play = false;
 			}
 			else {
-				table[check(table, col)][col] = 'O';
+				int row = check(table, col);
+				table[row][col] = 'O';
+				if(checkWin(table, row, col)) {
+					System.out.println("\nWINNER!");
+					win = true;
+				}
 				play = true;
 			}
 			
 			print(table);
+			if(win) {
+				break;
+			}
 		}
 
 	}
@@ -62,4 +75,235 @@ public class TestConnect {
 	    }
 	}
 
+	static boolean checkHorizontal(char[][] arr, int row, int col) {
+		boolean win  = false;
+		char piece = arr[row][col];
+		int piecesOwned = 1;
+		int width = arr[0].length;
+		boolean keepChecking = true;
+		int checkIndex = col - 1;
+		// Check left side
+		while(keepChecking) {
+			if(checkIndex >= 0) {
+				char checkPiece = arr[row][checkIndex];
+				if(checkPiece == piece) {
+					piecesOwned++;
+					checkIndex--;
+				}
+				else {
+					keepChecking = !keepChecking;
+				}
+			}
+			else {
+				keepChecking = !keepChecking;
+			}
+		}
+		
+		keepChecking = true;
+		checkIndex = col + 1;
+		// Check right side
+		while(keepChecking) {
+			if(checkIndex < width) {
+				char checkPiece = arr[row][checkIndex];
+				if(checkPiece == piece) {
+					piecesOwned++;
+					checkIndex++;
+				}
+				else {
+					keepChecking = !keepChecking;
+				}
+			}
+			else {
+				keepChecking = !keepChecking;
+			}
+		}
+		
+		if(piecesOwned >= 4) {
+			win = true;
+		}
+		
+		return win;
+	}
+	
+	static boolean checkVertical(char[][] arr, int row, int col) {
+		boolean win = false;
+		char piece = arr[row][col];
+		int piecesOwned = 1;
+		boolean keepChecking = true;
+		int height = arr.length;
+		
+		// Check below first
+		int checkIndex = row - 1;
+		while(keepChecking) {
+			if(checkIndex >= 0) {
+				char checkPiece = arr[checkIndex][col];
+				if(checkPiece == piece) {
+					piecesOwned++;
+					checkIndex--;
+				}
+				else {
+					keepChecking = !keepChecking;
+				}
+			}
+			else {
+				keepChecking = !keepChecking;
+			}
+		}
+		
+		// Checking above now
+		keepChecking = true;
+		checkIndex = row + 1;
+		while(keepChecking) {
+			if(checkIndex < height) {
+				char checkPiece = arr[checkIndex][col];
+				if(checkPiece == piece) {
+					piecesOwned++;
+					checkIndex++;
+				}
+				else {
+					keepChecking = !keepChecking;
+				}
+			}
+			else {
+				keepChecking = !keepChecking;
+			}
+		}
+		
+		if(piecesOwned >= 4) {
+			win = true;
+		}
+		
+		return win;
+	}
+	
+	// checks top-left to bottom-right diagonal
+	static boolean checkDiagonalTLBR(char[][] arr, int row, int col) {
+		boolean win = false;
+		
+		char piece = arr[row][col];
+		int piecesOwned = 1;
+		boolean keepChecking = true;
+		int height = arr.length;
+		int width = arr[0].length;
+		
+		// TOP-LEFT TO BOTTOM-RIGHT DIAGONAL CHECK
+		
+		int rowCheck = row + 1;
+		int colCheck = col - 1;
+		
+		// Checking toward top left first
+		while(keepChecking) {
+			if(rowCheck < height && colCheck >= 0) {
+				char checkPiece = arr[rowCheck][colCheck];
+				if(checkPiece == piece) {
+					piecesOwned++;
+					rowCheck++;
+					colCheck--;
+				}
+				else {
+					keepChecking = !keepChecking;
+				}
+			}
+			else {
+				keepChecking = !keepChecking;
+			}
+		}
+		
+		// Checking toward bottom right now
+		keepChecking = true;
+		rowCheck = row - 1;
+		colCheck = col + 1;
+		
+		while(keepChecking) {
+			if(rowCheck >= 0 && colCheck < height) {
+				char checkPiece = arr[rowCheck][colCheck];
+				if(checkPiece == piece) {
+					piecesOwned++;
+					rowCheck--;
+					colCheck++;
+				}
+				else {
+					keepChecking = !keepChecking;
+				}
+			}
+			else {
+				keepChecking = !keepChecking;
+			}
+		}
+		
+		if(piecesOwned >= 4) {
+			win = true;
+		}
+		
+		return win;
+	}
+	
+	// Bottom-left to top-right diagonal check
+	static boolean checkDiagonalBLTR(char[][] arr, int row, int col) {
+		boolean win = false;
+		char piece = arr[row][col];
+		int piecesOwned = 1;
+		boolean keepChecking = true;
+		int height = arr.length;
+		int width = arr[0].length;
+		
+		int rowCheck = row - 1;
+		int colCheck = col - 1;
+		
+		// Checking toward bottom-left first
+		while(keepChecking) {
+			if(rowCheck >= 0 && colCheck >= 0) {
+				char checkPiece = arr[rowCheck][colCheck];
+				if(checkPiece == piece) {
+					piecesOwned++;
+					rowCheck--;
+					colCheck--;
+				}
+				else {
+					keepChecking = !keepChecking;
+				}
+			}
+			else {
+				keepChecking = !keepChecking;
+			}
+		}
+		
+		keepChecking = true;
+		rowCheck = row + 1;
+		colCheck = col + 1;
+		// Checking toward top-right now
+		while(keepChecking) {
+			if(rowCheck < height && colCheck < width) {
+				char checkPiece = arr[rowCheck][colCheck];
+				if(checkPiece == piece) {
+					piecesOwned++;
+					rowCheck++;
+					colCheck++;
+				}
+				else {
+					keepChecking = !keepChecking;
+				}
+			}
+			else {
+				keepChecking = !keepChecking;
+			}
+		}
+		
+		if(piecesOwned >= 4) {
+			win = true;
+		}
+		
+		return win;
+	}
+	
+	static boolean checkWin(char[][] arr, int row, int col) {
+		boolean win = false;
+		
+		if(checkVertical(arr, row, col) || checkHorizontal(arr, row, col) || checkDiagonalTLBR(arr, row, col) || checkDiagonalBLTR(arr, row, col)) {
+			win = true;
+		}
+		
+		return win;
+	}
+	
 }
